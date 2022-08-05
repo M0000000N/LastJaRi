1f1b6a386419327935feff2de90465b826340a8e
roomMaker <leehuk0622@gmail.com>
2022-08-05 16:12:39 +0900

    Build

---
+++ LastJaRi/Assets/Script/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene(1,LoadSceneMode.Additive);
+        SceneManager.LoadScene(2,LoadSceneMode.Additive);
+        SceneManager.LoadScene(3,LoadSceneMode.Additive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
